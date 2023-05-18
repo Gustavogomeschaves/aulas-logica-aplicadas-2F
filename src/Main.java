ee94a35527bc48bb25d4cac4387f2dfc68a540a4
Gustavogomeschaves <cejutagu@gmail.com>
2023-05-18 08:27:44 -0300

    interessante

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Aula de Orientação  a Objetos
+        // Declarar Objetos
+        Pessoa adao;
+        // Instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+        // Definição do formato
+        adao.nome = "Adão";
+        adao.sobrenome = "silva";
+        eva.nome = "Eva";
+        eva.sobrenome = "silva";
+        // Acionar comportamento
+        adao.falar();
+        eva.falar();
     }
 }
